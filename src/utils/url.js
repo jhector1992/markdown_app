6512877f0793b7e59e028489e43b9f23b2c9b840
Hector Jauregui <jhector_1992@yahoo.com>
2020-07-22 00:38:29 -0500

    Heroku configs

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,7 +1,11 @@
 var URL = {
-    // Routes
-    app: 'http://192.168.100.11:3000',
-    api: 'http://192.168.100.11:4000',
+// Routes
+// LOCAL
+//    app: 'http://192.168.100.11:3000',
+//    api: 'http://192.168.100.11:4000',
+// HEROKU
+    app: 'https://markdown-app-react.herokuapp.com',
+    api: 'https://markdown-api.herokuapp.com',
 
     // Content
     markdown: '/markdown'
